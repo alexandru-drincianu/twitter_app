6ea6ab9c92cc6b1f5a739e8778ba9dc5b9f8241c
alex.drincianu <alexdrincianu@gmail.com>
2022-04-01 16:41:13 +0300

    Primul meu commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+    def hello 
+        render html: 'Hello World'
+    end
+
+    def alta_ruta
+        # render html: 'Ai intrat pe alta ruta'
+        
+    end
+end
